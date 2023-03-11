112b3ae2ce8b2d385a445a5cad8c3b1ccc65d813
hotaru <36354847+hotaru51@users.noreply.github.com>
2023-03-11 15:49:09 +0900

    メッセージPOST

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"net/http"
+	"os"
 
-	"github.com/hotaru51/slack-post-v2/messeage"
 	"github.com/hotaru51/slack-post-v2/config"
+	"github.com/hotaru51/slack-post-v2/messeage"
 )
 
 func main() {
-	fmt.Println(messeage.GenerateMessageJson())
-	fmt.Println(config.GetAbsPathOfExecutable())
-	fmt.Println(config.GetWebhookUrl().WebhookUrl)
-	fmt.Println(config.ReadConfigJson())
+	webhookUrl := config.GetWebhookUrl()
+	body := messeage.GenerateMessageJson()
+
+	resp, err := http.Post(webhookUrl.WebhookUrl, "application/json", bytes.NewBuffer([]byte(body)))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(body)
+	fmt.Println(resp.Status)
 }
